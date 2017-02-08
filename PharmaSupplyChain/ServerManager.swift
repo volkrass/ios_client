927707e5092ccffae99510bf1e5e5e7f6b76f032
YuryB <yurybelevskiy@gmail.com>
2017-02-08 18:55:42 +0100

    Added server call to request prepared shipment using TNT number

--- PharmaSupplyChain/ServerManager.swift
+++ PharmaSupplyChain/ServerManager.swift
@@ -15,8 +15,8 @@ class ServerManager {
     
     // MARK: Constants
     
-    //fileprivate let API_URL = "https://core.modum.io/api/"
-    fileprivate let API_URL = "http://dev.modum.io/api/"
+    fileprivate let CORE_API_URL = "https://core.modum.io/api/"
+    fileprivate let DEV_API_URL = "http://dev.modum.io/api/"
     
     /* If date from the server-side is to set to nil, server automatically sets this value */
     static let serverNilDateString: String = "0001-01-01T00:34:08+00:34"
@@ -41,13 +41,13 @@ class ServerManager {
     }
     
     /*
-     Given UserCredentials object and completionHandler, attempts to login the user with given credentials.
+     Given UserCredentials object and completionHandler, attempts to login with given credentials.
      If authentication was successful, executes completionHandler with no parameters
      Otherwise, executes completionHandler with the error occured.
     */
     func authenticateUser(WithCredentials loginCredentials: LoginCredentials, completionHandler: @escaping (AuthenticationError?) -> Void) {
         if let jsonLoginCredentials = loginCredentials.toJSON(), let parameters = ServerUtils.parameters(FromJSON: jsonLoginCredentials) {
-            Alamofire.request(API_URL + "login", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).validate().responseJSON(completionHandler: {
+            Alamofire.request(DEV_API_URL + "login", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).validate().responseJSON(completionHandler: {
                 [weak self]
                 response in
                 
@@ -86,24 +86,12 @@ class ServerManager {
     }
     
     /*
-     Extracts user ID from authentication token
-     If invalid authentication token is given, returns nil
+     Requests a list of user parcels from the server and persists received Parcel objects in CoreData
+     - @completionHandler returns a boolean variable, indicating whether request was successful or not
      */
-    fileprivate func extractUserID(FromAuthenticationToken authToken: String) -> Int? {
-        let splitToken = authToken.characters.split(separator: ".").map(String.init)
-        if let decodedData = splitToken[1].base64DecodedData() {
-            let authTokenJson = JSON(data: decodedData)
-            log("\(authTokenJson)")
-            return authTokenJson["userId"].int
-        } else {
-            return nil
-        }
-    }
-    
-    /* */
     func getUserParcels(completionHandler: @escaping (_ success: Bool) -> Void) {
         if let authorizationHeader = authorizationHeader {
-            Alamofire.request(API_URL + "v2/parcels/web", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization" : authorizationHeader]).validate().responseJSON(completionHandler: {
+            Alamofire.request(DEV_API_URL + "v2/parcels/web", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization" : authorizationHeader]).validate().responseJSON(completionHandler: {
                 [weak self]
                 response in
                 
@@ -127,6 +115,55 @@ class ServerManager {
             })
         } else {
             completionHandler(false)
+        }
+    }
+    
+    /*
+     Requests a prepared shipment for a specified Track-n-Trace number
+     - @tntNumber: Track-N-Trace number of the shipment
+     - @completionHandler: returns a boolean variable, indicating whether request was successful or not
+     */
+    func getPreparedShipment(WithTNTNumber tntNumber: String, WithCompletionHandler completionHandler: @escaping (_ success: Bool) -> Void) {
+        if let authorizationHeader = authorizationHeader {
+            Alamofire.request(DEV_API_URL + "preparedshipments/tntnumber/\(tntNumber)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization" : authorizationHeader]).validate().responseJSON(completionHandler: {
+                [weak self]
+                response in
+                
+                if let serverManager = self {
+                    switch response.result {
+                    case .success(let data):
+                        let responseData = JSON(data)
+                        if let parcels = responseData.array {
+                            for parcelJSON in parcels {
+                                let parcel = NSEntityDescription.insertNewObject(forEntityName: "Parcel", into: serverManager.coreDataManager.viewingContext) as! Parcel
+                                parcel.fromJSON(object: parcelJSON)
+                            }
+                            serverManager.coreDataManager.saveLocally(managedContext: serverManager.coreDataManager.viewingContext)
+                            completionHandler(true)
+                        }
+                    case .failure(let error):
+                        log("Error is \(error.localizedDescription)")
+                        completionHandler(false)
+                    }
+                }
+            })
+        }
+    }
+    
+    // MARK: Helper functions
+    
+    /*
+     Extracts and returns user ID from authentication token
+     If invalid authentication token is given, returns nil
+     */
+    fileprivate func extractUserID(FromAuthenticationToken authToken: String) -> Int? {
+        let splitToken = authToken.characters.split(separator: ".").map(String.init)
+        if let decodedData = splitToken[1].base64DecodedData() {
+            let authTokenJson = JSON(data: decodedData)
+            log("\(authTokenJson)")
+            return authTokenJson["userId"].int
+        } else {
+            return nil
         }
     }
     
